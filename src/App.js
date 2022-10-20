8155be559914ea05dcd24eb8cc526109169aee95
Parul Gupta <94689580+parulgupta2001@users.noreply.github.com>
2022-10-20 16:16:42 +0530

    feat : left nav

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
-import logo from "./logo.svg";
+import { LeftNav } from "./components/leftNav/LeftNav";
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <LeftNav />
+    </div>
+  );
 }
 
 export default App;
